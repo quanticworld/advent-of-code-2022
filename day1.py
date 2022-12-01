61d5112877eec2a7312c19281564032ee62839e8
quantic <quantic@lilalop.fr>
2022-12-01 15:48:35 +0100

    no preprocessing cheat-shit

--- day1.py
+++ day1.py
@@ -2,15 +2,12 @@ import timeit
 
 
 def part1():
-    f = open("day1.txt", "r")
-    lines = f.readlines()
-    return max(map(eval, lines))
-
+    data = open("day1.txt", "r").read().replace("\n\n", ",").replace("\n", "+")[:-1]
+    return max(map(eval, data.split(',')))
 
 def part2():
-    f = open("day1.txt", "r")
-    lines = f.readlines()
-    sorted_lines = list(map(eval, lines))
+    data = open("day1.txt", "r").read().replace("\n\n", ",").replace("\n", "+")[:-1]
+    sorted_lines = list(map(eval, data.split(',')))
     sorted_lines.sort()
     return sum(sorted_lines[-3:])
 
